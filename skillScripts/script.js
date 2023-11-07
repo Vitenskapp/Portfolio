28e9a7d89ab342c1ca7bd5943677da435f1efb98
Vinicius Alves <viniciusalvesms757@gmail.com>
2023-11-07 19:47:19 -0300

    Figma-Desc
    I add the description to the FIGMA box, html css and js

--- skillScripts/script.js
+++ skillScripts/script.js
@@ -36,11 +36,9 @@ function fecharDescCSS() {
     skillBox.style.position = "absolute";
 }
 
-var html = document.querySelector('img.css3');
-html.addEventListener('mouseenter', abrirDescCSS);
-html.addEventListener('mouseleave', fecharDescCSS);
-
-
+var css = document.querySelector('img.css3');
+css.addEventListener('mouseenter', abrirDescCSS);
+css.addEventListener('mouseleave', fecharDescCSS);
 
 function abrirDescJS() {
     var skillBox = document.querySelector('div.skillsBoxJS');
@@ -58,9 +56,29 @@ function fecharDescJS() {
     skillBox.style.position = "absolute";
 }
 
-var html = document.querySelector('img.javascript');
-html.addEventListener('mouseenter', abrirDescJS);
-html.addEventListener('mouseleave', fecharDescJS);
+var javasc = document.querySelector('img.javascript');
+javasc.addEventListener('mouseenter', abrirDescJS);
+javasc.addEventListener('mouseleave', fecharDescJS);
+
+function abrirDescFIG() {
+    var skillBox = document.querySelector('div.skillsBoxFIG');
+    skillBox.style.visibility = "visible";
+    skillBox.style.width = "350px";
+    skillBox.style.transition = "1s";
+    skillBox.style.position = "relative";
+}
+
+function fecharDescFIG() {
+    var skillBox =  document.querySelector('div.skillsBoxFIG');
+    skillBox.style.visibility = "hidden";
+    skillBox.style.width = "0px";
+    skillBox.style.transition = "0.7s";
+    skillBox.style.position = "relative";
+}
+
+var figma = document.querySelector('img.figma');
+figma.addEventListener('mouseenter', abrirDescFIG);
+figma.addEventListener('mouseleave', fecharDescFIG);
 
 
 
